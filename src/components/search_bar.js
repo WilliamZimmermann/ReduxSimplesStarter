ed4ff9abcad2338c69cbed2196652d8d4bea9568
WilliamZimmermann <zimmermann.william@outlook.com>
2018-05-31 17:09:18 -0300

    Componente Search

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SearchBar = function(){
+    return <input/>
+};
+
+// Any file that import this file (search_bar.js) will import the component SearchBar too
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Customized components
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Youtube API Key
+const API_KEY = 'AIzaSyCnhstiigCk-NDfJOEFAce-vBJQ95YX5iE';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce some HTML
+const App = function(){
+    return (
+        <div>
+            <SearchBar></SearchBar>
+        </div>
+    ); //This is a JSX nottaion
+}
+
+// Take this component's generated HTML and put it on th epage (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container')); 
+// In index.html we have a div named "container". React will put in that container our code
