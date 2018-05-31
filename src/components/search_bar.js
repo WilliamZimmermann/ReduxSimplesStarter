ac1064d06b0892c8fa77a8a785e7a7bc127695f6
WilliamZimmermann <zimmermann.william@outlook.com>
2018-05-31 18:06:20 -0300

    On change function to show the values

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,13 @@ import React, { Component } from 'react';
 class SearchBar extends Component{
     // This is a method called render, it's a little bit different of normal javascript methods
     render(){
-        return <input/>;
+        //This input, when change, will call our method onInputChange
+        return <input onChange={this.onInputChange} />;
+    }
+
+    //We will create another method, to be called when the input change
+    onInputChange(event){ //This method name could be quandoOInputMudar... Also the attribute
+        console.log(event.target.value); //Show in browser console the value
     }
 }
 
