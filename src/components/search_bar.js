87d626e120c3e58c393dd0cf062ac28d2eddbcee
WilliamZimmermann <zimmermann.william@outlook.com>
2018-05-31 17:47:39 -0300

    Now, the component was changed to a Class Component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = function(){
-    return <input/>
-};
+//We take all functionalities that React.Component class give us
+class SearchBar extends Component{
+    // This is a method called render, it's a little bit different of normal javascript methods
+    render(){
+        return <input/>;
+    }
+}
 
 // Any file that import this file (search_bar.js) will import the component SearchBar too
 export default SearchBar;
