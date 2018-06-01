f6e900d8cc26ebfc3caa72e121694e0b383bb2f9
WilliamZimmermann <zimmermann.william@outlook.com>
2018-05-31 22:47:46 -0300

    Now, we are showing a list of videos... without the video yet

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,9 +2,17 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = function(props){
+    // Here we are storyng a list of videos. We make a foreach using array.map (videos.map)
+    // for each video (videos) that he take, he create a var called video and pass thsi var
+    // for the component VideoList. 
+    const videoItems = props.videos.map((video)=>{
+        return <VideoListItem key={video.etag} video={video} />
+        // key will receive video.etag (etag it's a unic identifier for each video give by YouTube API)
+    });
+
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {videoItems}
         </ul>
     );
 }
